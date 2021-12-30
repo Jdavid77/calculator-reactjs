b3224bd1c5beb7e1f9f80a7a9ba61111891b5c50
Jdavid77 <jdavid72009@hotmail.com>
2021-12-30 12:52:22 +0000

    Finish

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,8 +3,18 @@ import "./Calculator.css";
 import Button from "../Components/Button";
 import Display from "../Components/Display";
 
+
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0,0],
+    current: 0
+}
+
 export default class Calculator extends React.Component{
 
+    state = { ...initialState}
 
     constructor(props) {
         super(props)
@@ -14,7 +24,7 @@ export default class Calculator extends React.Component{
     }
 
     clearMemory() {
-        console.log('limpar');
+        this.setState({...initialState})
     }
 
     setOperation(operation) {
@@ -22,14 +32,33 @@ export default class Calculator extends React.Component{
     }
 
     addDigit(n) {
-        console.log(n)
+        if(n === '.' && this.state.displayValue.includes(".")){
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === "0"
+            || this.state.clearDisplay
+
+        const currentValue = clearDisplay ? "" : this.state.displayValue
+        const displayValue = currentValue + n
+
+        this.setState({displayValue, clearDisplay:false})
+
+        if(n !== ".") {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue;
+            this.setState({values})
+            console.log(values)
+        }
     }
 
     render() {
 
         return (
             <div className="Calculator">
-                <Display value={100}/>
+                <Display value={this.state.displayValue}/>
                 <Button label="AC" click={this.clearMemory} triple/>
                 <Button label="/" click={this.setOperation} operation/>
                 <Button label="7" click={this.addDigit}/>
